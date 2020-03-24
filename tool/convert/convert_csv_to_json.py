df244556f4b4a9acff0cd53f9b16ccc82ea0a9a5
varusan <subaru@varu3.me>
2020-03-25 00:32:53 +0900

    patientsデータを生成するメソッドを追加した

--- tool/convert/convert_csv_to_json.py
+++ tool/convert/convert_csv_to_json.py
@@ -4,17 +4,44 @@ import json
 import csv
 import os
 import glob
+import sys
+import datetime
+import pandas as pd
+from pprint import pprint
 
-csv_files = glob.glob("../../static/data/*.csv")
-if len(csv_files) == 0: print("CSV data files are not found.")
+patients_DATA_CSV_FILE_NAME = "440001_oita_covid19_patients.csv"
 
-for cf in csv_files:
+def main():
+    patients_data_csv_file = "../../static/data/" + patients_DATA_CSV_FILE_NAME
+    if os.path.exists(patients_data_csv_file) == False:
+        print("CSV data files are not found.")
+        sys.exit(1)
+
+    data = import_csv_to_dict(patients_data_csv_file, encoding='utf_8_sig')
+    patients = generate_patients(data)
+
+
+def import_csv_to_dict(csv_file, encoding='utf_8_sig'):
     json_list = []
-    output_json = "../../data/" + os.path.splitext(os.path.basename(cf))[0] + ".json"
-    # csvのエンコード形式がutf-8 with BOMだったため
-    with open(cf, 'r', encoding='utf_8_sig') as f:
-        for row in csv.DictReader(f):
-            json_list.append(row)
-    
-    with open(output_json, 'w') as f:
-        json.dump(json_list, f, ensure_ascii=False, indent=2)
+    with open(csv_file, 'r', encoding=encoding) as f:
+        for row in csv.DictReader(f): json_list.append(row)
+    return json.loads(json.dumps(json_list))
+
+def generate_patients(data):
+    patients = {
+        "date": datetime.datetime.now().strftime("%Y/%m/%d %H:%M"),
+        "data": [],
+    }
+    for d in data:
+        p = {
+            "リリース日": d["公表_年月日"] + "T08:00:00",
+            "居住地": d["居住地"],
+            "年代": d["年代"],
+            "性別": d["性別"],
+            "退院": d["退院済フラグ"],
+            "date": d["公表_年月日"]
+        }
+        patients["data"].append(p)
+    return patients
+
+main()
