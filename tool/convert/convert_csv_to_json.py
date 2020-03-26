40df43755eded8981a599bae57faa948cc4989ef
varusan <subaru@varu3.me>
2020-03-26 12:35:44 +0900

    patients_summaryデータを生成するメソッドを追加した

--- tool/convert/convert_csv_to_json.py
+++ tool/convert/convert_csv_to_json.py
@@ -6,19 +6,38 @@ import os
 import glob
 import sys
 import datetime
-import pandas as pd
+import collections 
+from copy import deepcopy
 from pprint import pprint
 
 PATIENTS_DATA_CSV_FILE_NAME = "440001_oita_covid19_patients.csv"
 
 def main():
-    patients_data_csv_file = "../../static/data/" + PATIENTS_DATA_CSV_FILE_NAME
-    if os.path.exists(patients_data_csv_file) == False:
+    import_csv_file = os.path.dirname(__file__) + "/../../static/data/" + PATIENTS_DATA_CSV_FILE_NAME
+    export_json_file = os.path.dirname(__file__) + "/../../data/data.json"
+
+    if os.path.exists(import_csv_file) == False:
         print("CSV data files are not found.")
         sys.exit(1)
 
-    data = import_csv_to_dict(patients_data_csv_file, encoding='utf_8_sig')
+    data = import_csv_to_dict(import_csv_file, encoding='utf_8_sig')
     patients = generate_patients(data)
+    patients_summary = generate_patients_summary(data)
+
+    today_date_string = datetime.datetime.now().strftime("%Y/%m/%d %H:%M")
+    data_json = {
+        "patients": {
+            "date": today_date_string,
+            "data": patients,
+        },
+        "patients_summary": {
+            "date": today_date_string,
+            "data": patients_summary,
+        }
+    }
+
+    with open(export_json_file, 'w') as f:
+        json.dump(data_json, f, indent=2, ensure_ascii=False)
 
 
 def import_csv_to_dict(csv_file, encoding='utf_8_sig'):
@@ -28,10 +47,11 @@ def import_csv_to_dict(csv_file, encoding='utf_8_sig'):
     return json.loads(json.dumps(json_list))
 
 def generate_patients(data):
-    patients = {
-        "date": datetime.datetime.now().strftime("%Y/%m/%d %H:%M"),
-        "data": [],
-    }
+#    patients = 
+#        "date": datetime.datetime.now().strftime("%Y/%m/%d %H:%M"),
+#        "data": [],
+#    }
+    patients = []
     for d in data:
         p = {
             "リリース日": d["公表_年月日"] + "T08:00:00",
@@ -41,7 +61,48 @@ def generate_patients(data):
             "退院": d["退院済フラグ"],
             "date": d["公表_年月日"]
         }
-        patients["data"].append(p)
+        patients.append(p)
+
     return patients
+
+def generate_patients_summary(data):
+    count_date = [ datetime.datetime.strptime(d["公表_年月日"], '%Y-%m-%d') for d in data ]
+    start_date = sorted(count_date)[0]
+    end_date   = start_date + datetime.timedelta(days=len(count_date))
+
+    # 日付に対して値が0のデータを作る
+    df_date = {}
+    for i in daterange(start_date, end_date):
+        df_date[i] = 0
+
+    df_summary = {}
+    for date, total in collections.Counter(count_date).items():
+        df_summary[date] = total
+
+    df = deepmerge(df_date, df_summary)
+
+    patients_summary = []
+    for date, total in df.items():
+        ps = {
+            "日付": date.strftime("%Y-%m-%d"),
+            "小計": total,
+        }
+        patients_summary.append(ps)
+    
+    return patients_summary
+    
+
+def deepmerge(src, update):
+    result = deepcopy(src)
+    for k, v in update.items():
+        if k in result and isinstance(result[k], dict):
+            result[k] = deepmerge(result[k], v)
+        else:
+            result[k] = deepcopy(v)
+    return result
+
+def daterange(start_date, end_date):
+    for n in range((end_date - start_date).days):
+        yield start_date + datetime.timedelta(n)
 
 main()
