6c26d4b631c4070097fee6d8f63db8b82aaa7b3b
varusan <subaru@varu3.me>
2020-03-26 15:11:27 +0900

    pythonのおまじないを追加

--- tool/convert/convert_csv_to_json.py
+++ tool/convert/convert_csv_to_json.py
@@ -138,4 +138,5 @@ def daterange(start_date, end_date):
     for n in range((end_date - start_date).days + 1):
         yield start_date + datetime.timedelta(n)
 
-main()
+if __name__ == "__main__":
+    main()
