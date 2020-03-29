414221531e8a6398b7cc84ccdae1c03f3d728201
Shoma Okamoto <shoma.develop@gmail.com>
2020-03-29 13:10:22 +0900

    Display tmp AgeCard.

---
+++ components/cards/AgeCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-col cols="12" md="6" class="DataCard">
+    <circle-chart-for-age
+      :title="$t('年代別陽性患者数')"
+      :title-id="'patients-by-age'"
+      :chart-data="ageGraph"
+      :date="'2020-03-28'"
+      :unit="$t('床')"
+      :info="$t('入院患者数') + '/' + $t('総病床数')"
+      :url="'https://www.stopcovid19.jp/'"
+    />
+  </v-col>
+</template>
+
+<script>
+import formatVariableGraph from '@/utils/formatVariableGraph.ts'
+import CircleChartForAge from '@/components/CircleChartForAge.vue'
+import age from '@/data/age.json'
+
+export default {
+  components: {
+    CircleChartForAge
+  },
+  data() {
+    const ageGraph = formatVariableGraph(age.data)
+
+    const data = {
+      age,
+      ageGraph
+    }
+
+    return data
+  }
+}
+</script>
